1a546a882a920a8f472c18ceed6f4b4ad25dd8d2
NikoF <thanasisleventis1@gmail.com>
2025-01-23 00:05:35 -0500

    Add CSI3480_Assignment1.java with rectangle area calculation

---
+++ CSI3480_Assignment1.java
@@ -0,0 +1,9 @@
+public class CSI3480_Assignment1 {
+    public static void main(String[] args) {
+        int width = 5;  // width of the rectangle
+        int height = 7;  // height of the rectangle
+
+        int area = width * height;  // calculate the area
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
